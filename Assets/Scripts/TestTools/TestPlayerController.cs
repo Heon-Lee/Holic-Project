0c226b44fba499c29ac49db28eae9e990bca0c79
Heon-Lee <djamina81@naver.com>
2017-09-06 07:38:22 +0900

    UI 매니저 추가

--- Assets/Scripts/TestTools/TestPlayerController.cs
+++ Assets/Scripts/TestTools/TestPlayerController.cs
@@ -17,67 +17,74 @@ public class TestPlayerController : MonoBehaviour
 		avatarController = GetComponent<PlayerAvatarController>();
 	}
 
+
+    void Update()
+    {
+        
+    }
+
+
 	// Update is called once per frame
-	private void Update ()
-	{
-		OnArrowKeyCodeCalled();
+	//private void Update ()
+	//{
+	//	OnArrowKeyCodeCalled();
 
-		if (Input.GetKeyDown(KeyCode.Q))
-		{
-			Debug.Log("Compare the quest...");
-		}
+	//	if (Input.GetKeyDown(KeyCode.Q))
+	//	{
+	//		Debug.Log("Compare the quest...");
+	//	}
 
-		if (Input.GetKeyDown(KeyCode.KeypadEnter))
-		{
-			avatarController.TryAttack();
-		}
+	//	if (Input.GetKeyDown(KeyCode.KeypadEnter))
+	//	{
+	//		avatarController.TryAttack();
+	//	}
 
-		if (Input.GetMouseButtonDown(0))
-		{
-			RaycastHit hit;
-			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+	//	if (Input.GetMouseButtonDown(0))
+	//	{
+	//		RaycastHit hit;
+	//		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
-			if (Physics.Raycast(ray, out hit, Mathf.Infinity))
-			{
-				GameObject hitObject = hit.collider.gameObject;
+	//		if (Physics.Raycast(ray, out hit, Mathf.Infinity))
+	//		{
+	//			GameObject hitObject = hit.collider.gameObject;
 
-				if (hitObject.CompareTag("Enemy"))
-				{
-					EnemyObjectBase enemy = hitObject.GetComponent<EnemyObjectBase>();
-					//Debug.Log("You had selected " + enemy.attribute.nameTag);
-					enemy.OnHurt();
-				}
+	//			if (hitObject.CompareTag("Enemy"))
+	//			{
+	//				EnemyObjectBase enemy = hitObject.GetComponent<EnemyObjectBase>();
+	//				//Debug.Log("You had selected " + enemy.attribute.nameTag);
+	//				enemy.OnHurt();
+	//			}
 
-				if (hitObject.CompareTag("NPC"))
-				{
-					NPCObjectBase npc = hitObject.GetComponent<NPCObjectBase>();
-					npc.Interact();
-					//Debug.Log("You had selected " + npc.attribute.nameTag + ", the " + npc.attribute.classTag);
-				}
-			}
-		}
-	}
+	//			if (hitObject.CompareTag("NPC"))
+	//			{
+	//				NPCObjectBase npc = hitObject.GetComponent<NPCObjectBase>();
+	//				npc.Interact();
+	//				//Debug.Log("You had selected " + npc.attribute.nameTag + ", the " + npc.attribute.classTag);
+	//			}
+	//		}
+	//	}
+	//}
 
-	private void OnArrowKeyCodeCalled()
-	{
-		if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
-		{
-			transform.Translate(Vector3.left * speed * Time.deltaTime);
-		}
+	//private void OnArrowKeyCodeCalled()
+	//{
+	//	if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+	//	{
+	//		transform.Translate(Vector3.left * speed * Time.deltaTime);
+	//	}
 
-		if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
-		{
-			transform.Translate(Vector3.forward * speed * Time.deltaTime);
-		}
+	//	if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
+	//	{
+	//		transform.Translate(Vector3.forward * speed * Time.deltaTime);
+	//	}
 
-		if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
-		{
-			transform.Translate(Vector3.right * speed * Time.deltaTime);
-		}
+	//	if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+	//	{
+	//		transform.Translate(Vector3.right * speed * Time.deltaTime);
+	//	}
 
-		if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
-		{
-			transform.Translate(Vector3.back * speed * Time.deltaTime);
-		}
-	}
+	//	if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
+	//	{
+	//		transform.Translate(Vector3.back * speed * Time.deltaTime);
+	//	}
+	//}
 }
---
+++ Assets/Scripts/UI/UIManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour {
+
+    public static UIManager instance;
+
+    [SerializeField]
+    JoyStickController joystick;
+
+
+    [SerializeField]
+    Stack<GameObject> uiStack = new Stack<GameObject>();
+
+    [SerializeField]
+    GameObject test;
+
+    [SerializeField]
+    GameObject nowPopupUI;
+
+    void Awake()
+    {
+        instance = this;
+    }
+
+    public bool GetJoystick ()
+    {
+        return joystick.GetClick();
+    }
+
+    public void uiStackPush()
+    {
+        
+    }
+
+    public void uiStackPop()
+    {
+
+    }
+
+    public void uiStackClear()
+    {
+
+    }
+}
